9204ff1115fbc8632791d7b8466496b4cdf132e7
saralotano <saralotano@gmail.com>
2019-04-12 17:54:55 +0200

    Added broadcast communication

--- basestation.c
+++ basestation.c
@@ -9,39 +9,56 @@
 
 #define LOG_MODULE "App"
 #define LOG_LEVEL LOG_LEVEL_INFO
-#define SEND_INTERVAL (8 * CLOCK_SECOND)
-#define SEND_INTERVAL_BRO (13 * CLOCK_SECOND)
+//#define SEND_INTERVAL (8 * CLOCK_SECOND)
+//#define SEND_INTERVAL_BRO (13 * CLOCK_SECOND)
 
 
 
 
-
+/*
 static linkaddr_t dest_addr = {{ 0x0000, 0x0012, 0x004b, 0x0000, 0x000f, 0x0082, 0x0000, 0x0002 }};
 static int bro_period = 0;
 static int uni_period = 0;
+*/
 
+static linkaddr_t oven_addr;
 
 PROCESS(basestation_proc, "basestation");
 
 AUTOSTART_PROCESSES(&basestation_proc);
 
 static void input_callback(const void *data, uint16_t len, const linkaddr_t *src, const linkaddr_t *dest){
-	char received_data[strlen((char*)data)+1];
-	if(len == strlen((char*)data)+1){
-		memcpy(&received_data, data, strlen((char*)data)+1);
-		LOG_INFO("TIMESTAMP: %lu, Received \"%s\", from ", clock_seconds(), received_data);
-		LOG_INFO_LLADDR(src);
-		LOG_INFO_("\n");
+
+	char received_data[strlen((char *)data) + 1];
+	if(len == strlen((char *)data) + 1) {
+		memcpy(& received_data, data, strlen((char *)data) + 1);
+		if(strcmp(received_data, "oven")==0){
+			LOG_INFO("strcmp\n");
+			oven_addr = *src;
+			LOG_INFO("Oven address : ");
+			LOG_INFO_LLADDR(src);
+			LOG_INFO_("\n");
+		}
+		else if(strcmp(received_data, "windown")==0){
+			LOG_INFO("strcmp\n");
+			oven_addr = *src;
+			LOG_INFO("Windown address : ");
+			LOG_INFO_LLADDR(src);
+			LOG_INFO_("\n");
+		}
 	}
+
+
+	
 }
 
 PROCESS_THREAD(basestation_proc, ev, data){
-	static struct etimer periodic_timer;
-	static struct etimer broadcast_periodic_timer;
-	static char uni_msg[70];
-	static uint8_t * bro_msg;
+	//static struct etimer periodic_timer;
+	//static struct etimer broadcast_periodic_timer;
+	//static char uni_msg[70];
+	//static char bro_msg[70];
 	//sprintf(uni_msg, "Hello, I send you my msg at time: %d", uni_period);
-	//sprintf(bro_msg, "Hello, I am an annoying spammer, time: %d", bro_period);
+	//sprintf(bro_msg, "Discover message");
 
 	PROCESS_BEGIN();
 
@@ -76,16 +93,19 @@ PROCESS_THREAD(basestation_proc, ev, data){
 			bro_period+=13;
 		}
 	}*/
+		static uint8_t* bro_msg;
 
-	*bro_msg = DISCOVER_REQ;
-	nullnet_buf = bro_msg;
-	nullnet_len = 1;
-	NETSTACK_NETWORK.output(NULL);
+		*bro_msg = (uint8_t) DISCOVER_REQ;
+		nullnet_buf = (uint8_t*) bro_msg;
+		//nullnet_len = strlen(bro_msg)+1;
+		nullnet_len = sizeof(uint8_t);
+		NETSTACK_NETWORK.output(NULL);
 
-	LOG_INFO("Sending BROADCAST \"%d\" " , DISCOVER_REQ);
-	//LOG_INFO_LLADDR(&dest_addr);
-	LOG_INFO_("\n");
+		LOG_INFO("Sending BROADCAST \"%d\" " , (int) bro_msg);
+		//LOG_INFO_LLADDR(&dest_addr);
+		LOG_INFO_("\n");
 
 	PROCESS_END();
 }
+
 

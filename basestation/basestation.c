f5f75214101b90bd2b8373daf5519713cae6378f
saralotano <saralotano@gmail.com>
2019-04-27 18:22:01 +0200

    Added leds behaviour to the basestation and timer for broadcast attempts

--- basestation/basestation.c
+++ basestation/basestation.c
@@ -8,6 +8,7 @@
 #include "parameters.h"
 #include "sys/ctimer.h"
 #include "os/dev/serial-line.h"
+#include "os/dev/leds.h"
 //#include "arch/cpu/cc26x0-cc13x0/dev/cc26xx-uart.h"
 #define LOG_MODULE "App"
 #define LOG_LEVEL LOG_LEVEL_INFO
@@ -16,10 +17,17 @@
 static linkaddr_t oven_addr;
 static linkaddr_t window_addr;
 static struct ctimer broad_timer;
-bool oven_sync = false;
-bool window_sync = false;
-bool ovenBusy = false, windowBusy = false, basestationBusy = false;
-int num_sync_nodes = 0;				//
+static struct ctimer reactive_broad_timer;
+static struct ctimer green_led;
+static bool oven_sync = false;
+static bool window_sync = false;
+static bool ovenBusy = false, windowBusy = false, basestationBusy = false;
+static bool firstTime = true;
+static int num_sync_nodes = 0;	
+static int num_sync_attempts = 0;	
+
+void broadtimeCallback();
+
 PROCESS(basestation_proc, "basestation");
 
 AUTOSTART_PROCESSES(&basestation_proc);
@@ -43,11 +51,19 @@ static void synchNode(char* received_data,const linkaddr_t *src){
 			window_sync = true;
 			num_sync_nodes++;
 		}
-	
+
+	LOG_INFO("numero di nodi sincronizzati %d \n", num_sync_nodes);
 
 	if(num_sync_nodes == NUMBER_NODES){
-			LOG_INFO("numero di nodi sincronizzati raggiunto \n");
-			ctimer_stop(&broad_timer);
+		LOG_INFO("numero di nodi sincronizzati raggiunto \n");
+		ctimer_stop(&broad_timer);
+
+		leds_on(LEDS_GREEN);
+
+		if(!firstTime){
+			ctimer_stop(&green_led);
+			ctimer_stop(&reactive_broad_timer);		
+		}
 	}
 }
 
@@ -193,21 +209,58 @@ void handle_serial_line(char* data){
 
 }
 
-void discoverNodes(){
+void blinkGreenLedCallback(){
+	LOG_INFO("dentro blinkGreenLedCallback \n");
+	leds_toggle(LEDS_GREEN);
+	ctimer_restart(&green_led);
+}
 
+
+void discoverNodes(){
 	sendMsg(DISCOVER_REQ,NULL,NULL);
+	num_sync_attempts++;
+	LOG_INFO("numero tentativi discoverNodes: %d\n", num_sync_attempts);
+
+	leds_toggle(LEDS_RED);
+
+	if(num_sync_attempts == MAX_NUM_ATTEMPTS){
+		num_sync_attempts = 0;
+		LOG_INFO("fine tentativi di broadcast MAX_NUM_ATTEMPTS\n");
+		ctimer_stop(&broad_timer);
+		leds_off(LEDS_RED);
+
+		if(num_sync_nodes > 0){		
+			leds_on(LEDS_GREEN);
+			if(firstTime)
+				ctimer_set(&green_led, CLOCK_SECOND, blinkGreenLedCallback, NULL);
+		}
+
+		if(firstTime){
+			ctimer_set(&reactive_broad_timer, 5*CLOCK_SECOND, broadtimeCallback, NULL);	//cambiare in minuti
+			firstTime = false;
+		}		
+		else
+			ctimer_reset(&reactive_broad_timer);	
+	}
+	else
+		ctimer_reset(&broad_timer);
+}
+
+void broadtimeCallback(){
+	ctimer_restart(&broad_timer);
 }
 
 PROCESS_THREAD(basestation_proc, ev, data){
 
 	PROCESS_BEGIN();
 
+	leds_on(LEDS_RED);
+
 	//cc26xx_uart_set_input(serial_line_input_byte);
 	//serial_line_init();
 
 	nullnet_set_input_callback(input_callback);
-
-	ctimer_set(&broad_timer,2*CLOCK_SECOND,discoverNodes,NULL);
+	ctimer_set(&broad_timer,CLOCK_SECOND,discoverNodes,NULL);
 
 	while(1){
 		PROCESS_WAIT_EVENT_UNTIL(ev == serial_line_event_message);
--- window/build/sky/obj/heapmem.d
+++
@@ -1,4 +0,0 @@
-build/sky///obj/heapmem.o: ../../../../os/lib/heapmem.c \
- ../../../../os/lib/heapmem.h
-../../../../os/lib/heapmem.c :
- ../../../../os/lib/heapmem.h :
